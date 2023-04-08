2dc384c19617697fefb8464a6887c97cda366a96
Vibhanshu Sharma <101695282+vibhanshushrm2025@users.noreply.github.com>
2023-04-08 18:07:02 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
-
+console.log("models");
 const UserSchema = new Schema({
     email: {
         type: String,
